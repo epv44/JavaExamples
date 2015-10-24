4fda7048776209fb29b9dc267b4f310f0f2db5c3
Eric <epv9@case.edu>
2015-10-24 08:18:22 -0400

    completed chapter six

--- src/chapterSix/PrimeNumberCollector.java
+++ src/chapterSix/PrimeNumberCollector.java
@@ -1,6 +1,8 @@
 package chapterSix;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,6 +10,7 @@ import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Collector;
 
@@ -24,26 +27,46 @@ Map<Boolean, List<Integer>>>{
 
     @Override
     public BiConsumer<Map<Boolean, List<Integer>>, Integer> accumulator() {
-        // TODO Auto-generated method stub
-        return null;
+        return (Map<Boolean, List<Integer>> acc, Integer candidate) -> {
+            acc.get( isPrime(acc.get(true), candidate) ).add(candidate);
+        };
     }
 
     @Override
     public BinaryOperator<Map<Boolean, List<Integer>>> combiner() {
-        // TODO Auto-generated method stub
-        return null;
+        return (Map<Boolean, List<Integer>> map1,
+                Map<Boolean, List<Integer>> map2) -> {
+                    map1.get(true).addAll(map2.get(true));
+                    map1.get(false).addAll(map2.get(false));
+                    return map1;
+                };
     }
 
     @Override
     public Function<Map<Boolean, List<Integer>>, Map<Boolean, List<Integer>>> finisher() {
-        // TODO Auto-generated method stub
-        return null;
+        return Function.identity();
     }
 
     @Override
     public Set<java.util.stream.Collector.Characteristics> characteristics() {
-        // TODO Auto-generated method stub
-        return null;
+        return Collections.unmodifiableSet(EnumSet.of(Collector.Characteristics.IDENTITY_FINISH));
     }
 
+    public static boolean isPrime(List<Integer> primes, int candidate){
+        //optimize by checking the square root of the candidate
+        int candidateRoot = (int) Math.sqrt((double) candidate);
+        return takeWhile(primes, i -> i <= candidateRoot).stream()
+                .noneMatch(p -> candidate % p ==0);
+    }
+
+    public static <A> List<A> takeWhile(List<A> list, Predicate<A> p){
+        int i = 0;
+        for (A item: list) {
+            if (!p.test(item)){
+                return list.subList(0, i);
+            }
+            i++;
+        }
+        return list;
+    }
 }
